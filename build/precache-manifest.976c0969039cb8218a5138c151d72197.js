65b812e6883df09fe8da08240e9c51235b3cf8b9
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-08-01 16:58:02 +0530

    portfolio added

---
+++ build/precache-manifest.976c0969039cb8218a5138c151d72197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b40bd288516e2d141a50aa7dd1c6e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff206290715d7f70d1b",
+    "url": "/static/css/main.b50865a3.chunk.css"
+  },
+  {
+    "revision": "791d39c054cc3e452160",
+    "url": "/static/js/2.d5dce6d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5dce6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff206290715d7f70d1b",
+    "url": "/static/js/main.b314dabd.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
--- src/Componemts/About.js
+++ src/Componemts/About.js
@@ -3,7 +3,7 @@ const About= ()=> {
     return (
         <div className="my-5">
             <p>Github Finder is an React App  that allows  to find the github users and watch their profile </p>
-            <p>Developed by Hirdesh Gupta</p>
+            <p> <a href="https://myportfoliohirdeshgupta.herokuapp.com/index.html#"> Developed by Hirdesh Gupta </a></p>
         </div>
     )
 }
